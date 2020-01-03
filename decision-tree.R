9825c6563c141f534f985c4411f515137c782a24
KedraMichal <52571532+KedraMichal@users.noreply.github.com>
2020-01-03 21:15:06 +0100

    decison tree, random forest

--- decision-tree.R
+++ decision-tree.R
@@ -2,6 +2,7 @@ library(rpart)
 library(dplyr)
 library(caret)
 library(rpart.plot)
+library(caTools)
 
 cars <- read.csv("C:/Users/User/Desktop/R/ML/cars.csv")
 
@@ -10,17 +11,26 @@ str(cars)
 cars$origin<- factor(cars$origin, labels = c("USA", "Europe", "Japan"))
 
 
-cars_split<- sample(2, nrow(cars), prob = c(0.7, 0.3), replace = TRUE)
+cars_split<- sample.split(cars$origin, SplitRatio = 0.8)
 cars_split
-train_data<- cars[cars_split ==1,]
-test_data<- cars[cars_split ==2,]
+train_data<- subset(cars, cars_split == TRUE)
+test_data<- subset(cars, cars_split == FALSE)
 
 model<- rpart(train_data$origin~., train_data)
-
+model
 #vis
 prp(model)
 
 predict_mod<- predict(model, test_data, type  ="class")
-predict_tab<- confusionMatrix(table(predict_mod, test_data$origin))
+predict_tab<- table(predict_mod, test_data$origin)
 
 confusionMatrix(predict_tab)
+
+
+library(randomForest)
+rf_model<- randomForest(train_data$origin~., train_data)
+importance(rf_model)
+
+predict_rf <- predict(rf_model, test_data)
+
+confusionMatrix(table(predict_rf, test_data$origin))
